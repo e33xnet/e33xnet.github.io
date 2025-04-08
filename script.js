3abd524f138388c41b76533bcf1f5d1061d2a108
e33xnet <e33.xnet@gmail.com>
2025-04-09 00:21:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ document.getElementById('loginBtn').addEventListener('click', function() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Перевірка правильності введеного логіну та пароля
+    
     if (username === 'u5erdv' && password === 'xnet.33:(') {
-        // Якщо логін і пароль правильні, приховуємо форму входу та показуємо вміст
-        document.querySelector('.login-container').style.display = 'none';
-        document.getElementById('hiddenContent').style.display = 'block';
+        
+        window.location.href = 'welcome.html';
     } else {
         alert('Login error');
     }
