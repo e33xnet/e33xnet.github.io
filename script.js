9b311274028edc0deee89d1bcca38c57869e1e61
e33xnet <e33.xnet@gmail.com>
2025-04-08 23:55:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function attemptLogin() {
+document.getElementById('loginBtn').addEventListener('click', function() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    
-    // Просте перевірка логіна і пароля (можна замінити на реальну базу даних)
-    if (username === "admin" && password === "12345") {
-        document.getElementById('statusMessage').textContent = "Login successful!";
-        // Тут можна додати редірект на захищену сторінку
-        window.location.href = "/protected.html"; // Переходить на захищену сторінку
+
+    // Простий перевірка логіну і пароля
+    if (username === 'u5erdv' && password === 'xnet.33:()') {
+        document.querySelector('.login-container').style.display = 'none';
+        document.getElementById('hiddenContent').style.display = 'block';
     } else {
-        document.getElementById('statusMessage').textContent = "Invalid username or password.";
+        alert('Login error');
     }
-}
+});
