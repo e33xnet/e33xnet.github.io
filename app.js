91c8dc4db4f72d16b377ec7c90b927938302358c
e33xnet <e33.xnet@gmail.com>
2025-04-09 03:24:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 document.getElementById('loginBtn').addEventListener('click', function () {
-    const username = document.getElementById('username').value.trim().toLowerCase();
-    const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value.trim().toLowerCase();
+  const password = document.getElementById('password').value;
 
-    if (username === 'u5erdv' && password === 'xnet.33:(') {
-        document.querySelector('.login-container').style.display = 'none';
-        window.location.href = 'welcome.html';
-    } else {
-        const errorMsg = document.getElementById('error-msg');
-        errorMsg.style.display = 'block';
-        document.getElementById('username').value = '';
-        document.getElementById('password').value = '';
-    }
+  if (username === 'u5erdv' && password === 'xnet.33:(') {
+    window.location.href = 'welcome.html';
+  } else {
+    document.getElementById('error-msg').style.display = 'block';
+    document.getElementById('username').value = '';
+    document.getElementById('password').value = '';
+  }
 });
