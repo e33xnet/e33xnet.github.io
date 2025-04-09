54725b1a7a926e7aee26368d7c354fd444b0c021
e33xnet <e33.xnet@gmail.com>
2025-04-09 12:46:53 +0200

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,21 +1,36 @@
-const audioInput = document.getElementById('audioInput');
-const audioPlayer = document.getElementById('audioPlayer');
-audioInput.addEventListener('change', function () {
-    const file = this.files[0];
-    if (file) {
-        const url = URL.createObjectURL(file);
-        audioPlayer.src = url;
-        audioPlayer.play();
-    }
+function generateRandomName(ext) {
+  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  let name = '';
+  for (let i = 0; i < 10; i++) {
+    name += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return name + '.' + ext;
+}
+
+document.getElementById('audioUpload').addEventListener('change', function (e) {
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  const audioPlayer = document.getElementById('audioPlayer');
+  const randomName = generateRandomName(file.name.split('.').pop());
+
+  const audio = document.createElement('audio');
+  audio.controls = true;
+  audio.src = url;
+  audio.setAttribute('data-name', randomName);
+  audioPlayer.innerHTML = '';
+  audioPlayer.appendChild(audio);
 });
 
-const videoInput = document.getElementById('videoInput');
-const videoPlayer = document.getElementById('videoPlayer');
-videoInput.addEventListener('change', function () {
-    const file = this.files[0];
-    if (file) {
-        const url = URL.createObjectURL(file);
-        videoPlayer.src = url;
-        videoPlayer.play();
-    }
+document.getElementById('videoUpload').addEventListener('change', function (e) {
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  const videoPlayer = document.getElementById('videoPlayer');
+  const randomName = generateRandomName(file.name.split('.').pop());
+
+  const video = document.createElement('video');
+  video.controls = true;
+  video.src = url;
+  video.setAttribute('data-name', randomName);
+  videoPlayer.innerHTML = '';
+  videoPlayer.appendChild(video);
 });
