e146431962f8fad1cec58056b0b636786664a4f4
e33xnet <e33.xnet@gmail.com>
2025-04-09 17:23:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById('loginBtn').addEventListener('click', function() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'u5erdv' && password === 'xnet.33:(') {
+    if (username === 'U5ERDV' && password === 'xnet.33:(') {
         window.location.href = 'welcome.html';
     } else {
         document.getElementById('error').textContent = 'Login failed';
